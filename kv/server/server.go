2e303894e9efae6dff788e632b30b6b6729642fa
wanghao <wanghao@ubuntu.localdomain>
2024-12-16 16:48:37 +0800

    feat(project4b):完成project4b相关功能

--- kv/server/server.go
+++ kv/server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/kv/storage/raft_storage"
 	"github.com/pingcap-incubator/tinykv/kv/transaction/latches"
+	"github.com/pingcap-incubator/tinykv/kv/transaction/mvcc"
 	coppb "github.com/pingcap-incubator/tinykv/proto/pkg/coprocessor"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/tinykvpb"
@@ -50,17 +51,205 @@ func (server *Server) Snapshot(stream tinykvpb.TinyKv_SnapshotServer) error {
 // Transactional API.
 func (server *Server) KvGet(_ context.Context, req *kvrpcpb.GetRequest) (*kvrpcpb.GetResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	//给req中的keys上latch锁，避免并发问题。记得释放latch锁
+	keysToLatch := make([][]byte, 0)
+	keysToLatch = append(keysToLatch, req.Key)
+	server.Latches.WaitForLatches(keysToLatch)
+	defer server.Latches.ReleaseLatches(keysToLatch)
+
+	//执行获取key的value值操作。获取前需要判断该key是否已经被其他事务锁住，这里是通过获取key的lock结构体判断，而非latch
+	resp := new(kvrpcpb.GetResponse)
+	
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return resp, err
+	}
+	defer reader.Close()
+
+	mvccTxn := mvcc.NewMvccTxn(reader, req.Version)
+
+	//检查key的lock，看key是否已经被其他事务锁住
+	lock, err := mvccTxn.GetLock(req.Key)
+	if err != nil {
+		return resp, err
+	}
+
+	if lock != nil && lock.IsLockedFor(req.Key, req.Version, resp) {
+		return resp, nil
+	}
+
+	//key没有被其他事务锁住，获取key对应的value值
+	value, err := mvccTxn.GetValue(req.Key)
+	if err != nil {
+		return resp, err
+	}
+	if value == nil {
+		resp.NotFound = true
+	}
+	resp.Value = value
+	return resp, nil
 }
 
 func (server *Server) KvPrewrite(_ context.Context, req *kvrpcpb.PrewriteRequest) (*kvrpcpb.PrewriteResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	//给req中的keys上latch锁，避免并发问题。记得释放latch锁
+	keysToLatch := make([][]byte, 0)
+	for _, mutaion := range req.Mutations {
+		keysToLatch = append(keysToLatch, mutaion.Key)
+	}
+	server.Latches.WaitForLatches(keysToLatch)
+	defer server.Latches.ReleaseLatches(keysToLatch)
+
+	//执行预写操作。执行前需要判断1.预写的这些 key 的最新 Write，如果存在，且其 commitTs 大于当前事务的 startTs，说明存在 write conflict
+	//2.预写的这些key是否已经被其他事务锁住，这里是通过获取key的lock结构体判断，而非latch。
+	resp := new(kvrpcpb.PrewriteResponse)
+
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return resp, err
+	}
+	defer reader.Close()
+	
+	mvccTxn := mvcc.NewMvccTxn(reader, req.StartVersion)
+
+	//检查1
+	for _, mutation := range req.Mutations {
+		write, commitTs, err := mvccTxn.MostRecentWrite(mutation.Key)
+		if err != nil {
+			return resp, err
+		}
+
+		if write != nil && commitTs > req.StartVersion {
+			resp.Errors = append(resp.Errors, &kvrpcpb.KeyError{
+				Conflict: &kvrpcpb.WriteConflict{
+					StartTs:  req.StartVersion,
+					ConflictTs: commitTs,
+					Key:      mutation.Key,
+					Primary:   req.PrimaryLock,
+				},
+			})
+			return resp, nil
+		}
+
+	}
+
+	//检查2
+	for _, mutation := range req.Mutations {
+		lock, err := mvccTxn.GetLock(mutation.Key)
+		if err != nil {
+			return resp, err
+		}
+
+		if lock != nil && lock.Ts <= req.StartVersion {
+			resp.Errors = append(resp.Errors, &kvrpcpb.KeyError{
+				Locked: &kvrpcpb.LockInfo{
+					Key:         mutation.Key,
+					PrimaryLock: lock.Primary,
+					LockVersion: lock.Ts,
+					LockTtl:     lock.Ttl,
+				},
+			})
+			return resp, nil
+		}
+	}
+
+	//检查通过，执行预写(预写==写入default和写入lock)
+	for _, mutation := range req.Mutations {
+		var writeKind mvcc.WriteKind
+		switch mutation.Op {
+		case kvrpcpb.Op_Put:
+			mvccTxn.PutValue(mutation.Key, mutation.Value)
+			writeKind = mvcc.WriteKindPut
+		case kvrpcpb.Op_Del:
+			mvccTxn.DeleteValue(mutation.Key)
+			writeKind = mvcc.WriteKindDelete
+		case kvrpcpb.Op_Rollback:
+			writeKind = mvcc.WriteKindRollback
+		}
+
+		mvccTxn.PutLock(mutation.Key, &mvcc.Lock{
+			Kind: writeKind,
+			Ts: mvccTxn.StartTS,
+			Primary: req.PrimaryLock,
+			Ttl: req.LockTtl,
+		})
+	}
+
+	err = server.storage.Write(req.Context, mvccTxn.Writes())
+	if err != nil {
+		return resp, err
+	}
+	return resp, nil
 }
 
 func (server *Server) KvCommit(_ context.Context, req *kvrpcpb.CommitRequest) (*kvrpcpb.CommitResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	//给req中的keys上latch锁，避免并发问题。记得释放latch锁
+	keysToLatch := make([][]byte, 0)
+	keysToLatch = append(keysToLatch, req.Keys...)
+	server.Latches.WaitForLatches(keysToLatch)
+	defer server.Latches.ReleaseLatches(keysToLatch)
+
+	//执行提交操作。执行前需要检查 Lock.StartTs 和当前事务的 startTs 是否一致
+	resp := new(kvrpcpb.CommitResponse)
+
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return resp, err
+	}
+	defer reader.Close()
+	
+	mvccTxn := mvcc.NewMvccTxn(reader, req.StartVersion)
+
+	for _, key := range req.Keys {
+		lock, err := mvccTxn.GetLock(key)
+		if err != nil {
+			return resp, err
+		}
+		//重复提交和回滚提交时lock为空
+		if lock == nil {
+			write, _, err := mvccTxn.CurrentWrite(key)
+			if err != nil {
+				return resp, err
+			}
+
+			if write != nil && write.StartTS == mvccTxn.StartTS && write.Kind == mvcc.WriteKindRollback {
+				resp.Error = &kvrpcpb.KeyError{
+					Retryable: "true",
+				}
+				return resp, nil
+			}
+		} else if lock.Ts != mvccTxn.StartTS {
+			resp.Error = &kvrpcpb.KeyError{
+				Retryable: "true",
+			}
+			return resp, nil
+		}
+
+	}
+
+	//检查通过，执行提交(写入write和移除lock)
+	for _, key := range req.Keys {
+		lock, err := mvccTxn.GetLock(key)
+		if err != nil {
+			return resp, err
+		}
+		if lock == nil { //重复提交时lock为空
+			continue
+		}
+		mvccTxn.PutWrite(key, req.CommitVersion, &mvcc.Write{
+			Kind: lock.Kind,
+			StartTS: mvccTxn.StartTS,
+		})
+
+		mvccTxn.DeleteLock(key)
+	}
+
+	err = server.storage.Write(req.Context, mvccTxn.Writes())
+	if err != nil {
+		return resp, err
+	}
+	return resp, nil
 }
 
 func (server *Server) KvScan(_ context.Context, req *kvrpcpb.ScanRequest) (*kvrpcpb.ScanResponse, error) {
